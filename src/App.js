eaa91fc43db1d2fd112f76d15c43550c697a10d0
Terry <t3rrygong@gmail.com>
2020-07-23 20:38:33 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/about";
 import Portfolio from "./pages/portfolio";
 import Resume from "./pages/resume";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/resume" component={Resume} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
