d17294358d9dc6f55eb56ba4ddf3b6b5f05aa43a
Terry <t3rrygong@gmail.com>
2020-08-21 19:34:26 -0600

    Updates

---
+++ precache-manifest.c93411722407f6628694da57d6b3173d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0eb0cea49c8bc48b999ec0d47e927d5",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "9bb28a836839953626f5",
+    "url": "/React_Portfolio/static/css/main.5f0e1e5f.chunk.css"
+  },
+  {
+    "revision": "2d8f0baba30c627f3f2e",
+    "url": "/React_Portfolio/static/js/2.cdc9e95c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Portfolio/static/js/2.cdc9e95c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb28a836839953626f5",
+    "url": "/React_Portfolio/static/js/main.647dc0f1.chunk.js"
+  },
+  {
+    "revision": "3a0598dfa4517aaf1428",
+    "url": "/React_Portfolio/static/js/runtime-main.446e9683.js"
+  },
+  {
+    "revision": "5eb27b6765b5a8a884699f2ca4da76e6",
+    "url": "/React_Portfolio/static/media/1000px-Apple_logo_black.svg.5eb27b67.png"
+  },
+  {
+    "revision": "0abd5ac509e34d62087df764176dad27",
+    "url": "/React_Portfolio/static/media/1515037475950.0abd5ac5.jpg"
+  },
+  {
+    "revision": "bc852529297f5c23b98b4c6d247ba0fe",
+    "url": "/React_Portfolio/static/media/49275-recoil-windows-front-cover.bc852529.jpg"
+  },
+  {
+    "revision": "ccd588e194ad720f2409719facfa501a",
+    "url": "/React_Portfolio/static/media/Beyond_Two_Souls_final_cover.ccd588e1.jpg"
+  },
+  {
+    "revision": "83a42f99207c153fea85e4e8f4b4481f",
+    "url": "/React_Portfolio/static/media/CSGO-Logo.83a42f99.png"
+  },
+  {
+    "revision": "ec6c181a93cac54aeadff52b7add05ee",
+    "url": "/React_Portfolio/static/media/Chaos_Rings_logo.ec6c181a.png"
+  },
+  {
+    "revision": "265b71b51f3e709fe4051114b24a6667",
+    "url": "/React_Portfolio/static/media/Cncts-win-cover.265b71b5.jpg"
+  },
+  {
+    "revision": "4346eea8404ed5ebd917412a3c774693",
+    "url": "/React_Portfolio/static/media/Dead_Space_Box_Art.4346eea8.jpg"
+  },
+  {
+    "revision": "4835db30f8ff0255605e6d7c1cb1371f",
+    "url": "/React_Portfolio/static/media/Detroit_Become_Human.4835db30.jpg"
+  },
+  {
+    "revision": "7e2bf3c4c6653fe33468b7cf8312d6cc",
+    "url": "/React_Portfolio/static/media/Fate_Grand_Order_logo.7e2bf3c4.png"
+  },
+  {
+    "revision": "7b95aef212b971999e3426a0929b2085",
+    "url": "/React_Portfolio/static/media/Hatsune_Miku_Project_DIVA_Logo.7b95aef2.png"
+  },
+  {
+    "revision": "7ad402e64d6d21e3c55b5997926ab8f0",
+    "url": "/React_Portfolio/static/media/Llsiflogo.7ad402e6.png"
+  },
+  {
+    "revision": "515d9ae0fc4bef1fe07288d3f0954e7e",
+    "url": "/React_Portfolio/static/media/Need_for_Speed_Most_Wanted_Box_Art.515d9ae0.jpg"
+  },
+  {
+    "revision": "24a6e516deddfd1903dfc810eb02f303",
+    "url": "/React_Portfolio/static/media/Nier_Automata_cover_art.24a6e516.jpg"
+  },
+  {
+    "revision": "b4898af02a42891286acc5919ca3d097",
+    "url": "/React_Portfolio/static/media/Pokemon_Yellow.b4898af0.png"
+  },
+  {
+    "revision": "e6ce256f6e9c9abe4eda4b7b585f9cfd",
+    "url": "/React_Portfolio/static/media/Resume.e6ce256f.pdf"
+  },
+  {
+    "revision": "aacb96f16667ae245cd12fb6fce30773",
+    "url": "/React_Portfolio/static/media/Saya_no_Uta_cover.aacb96f1.jpg"
+  },
+  {
+    "revision": "64d08a1caf28deae44f90889f603fc84",
+    "url": "/React_Portfolio/static/media/Steins;Gate_cover_art.64d08a1c.jpg"
+  },
+  {
+    "revision": "07ea81180581a4afa8d2e3b04038f61e",
+    "url": "/React_Portfolio/static/media/Talesoftheabyss_us.07ea8118.jpg"
+  },
+  {
+    "revision": "34bf50e1980a98d78582543cbd2c8491",
+    "url": "/React_Portfolio/static/media/Video_Game_Cover_-_The_Last_of_Us.34bf50e1.jpg"
+  },
+  {
+    "revision": "f6eb5be1b3e87ed642ddbd08edd634e6",
+    "url": "/React_Portfolio/static/media/celebrity_twin.f6eb5be1.png"
+  },
+  {
+    "revision": "adaf01ef6434a7fd10a7db40b2550e37",
+    "url": "/React_Portfolio/static/media/googlebooks.adaf01ef.gif"
+  },
+  {
+    "revision": "d5b8f98b64f116fd6258bbab499a25cc",
+    "url": "/React_Portfolio/static/media/html_quiz.d5b8f98b.png"
+  },
+  {
+    "revision": "d09e16d29a3f30489df9dc0882ad4d48",
+    "url": "/React_Portfolio/static/media/linux.d09e16d2.png"
+  },
+  {
+    "revision": "d3d20324b52ee0ab817cffef5fb64c76",
+    "url": "/React_Portfolio/static/media/playstore-512.d3d20324.png"
+  },
+  {
+    "revision": "bdae4f928b31781e8df4053b589ab84d",
+    "url": "/React_Portfolio/static/media/quantumbreak.bdae4f92.jpg"
+  },
+  {
+    "revision": "becf747aef14589466423a973aa076ad",
+    "url": "/React_Portfolio/static/media/selfie.becf747a.png"
+  },
+  {
+    "revision": "26f25ef179479a699af1140e51dfeb3b",
+    "url": "/React_Portfolio/static/media/weather_dashboard.26f25ef1.png"
+  },
+  {
+    "revision": "9b39367c47a5654eda54be4ba2fd1436",
+    "url": "/React_Portfolio/static/media/work_day_scheduler.9b39367c.png"
+  },
+  {
+    "revision": "7b093c0f5a06b7a8e34c24410da9649c",
+    "url": "/React_Portfolio/static/media/xbox (1).7b093c0f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_Portfolio/precache-manifest.3f0529b1a99f7399a8b9fd3873ae330a.js"
+  "/React_Portfolio/precache-manifest.c93411722407f6628694da57d6b3173d.js"
 );
 
 self.addEventListener('message', (event) => {
