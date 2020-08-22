ffc69f7bd0c7b851e7832188c1a004499f20470b
Terry <t3rrygong@gmail.com>
2020-08-21 19:32:23 -0600

    update google books search image

--- src/components/images.js
+++ src/components/images.js
@@ -2,7 +2,7 @@ import CelebrityTwinImage from "../images/project_screenshots/celebrity_twin.png
 import WeatherDashboardImage from "../images/project_screenshots/weather_dashboard.png";
 import QuizImage from "../images/project_screenshots/html_quiz.png";
 import WorkDaySchedulerImage from "../images/project_screenshots/work_day_scheduler.png";
-import GoogleBooksSearchImage from "../images/project_screenshots/google_books_search.png";
+import GoogleBooksSearchImage from "../images/project_screenshots/googlebooks.gif";
 
 const images = {
     CelebrityTwinImage: CelebrityTwinImage,
