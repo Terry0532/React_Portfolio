2fdceffee9f7900a201b79c9cd783eae8fecafb9
Terry <t3rrygong@gmail.com>
2020-08-22 16:23:55 -0600

    update resume and project. removed extra image.

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -17,22 +17,20 @@ function Resume() {
                             <p>Back-end: NodeJS, ExpressJS, MySQL, Sequelize, MongoDB, Mongoose</p>
                             <h3><b>Projects</b></h3>
                             <h6><i><b>GOOGLE BOOKS SEARCH</b></i></h6>
-                            <p>A MERN(MySQL, Express, React, NodeJS) stack application that utilize Google Books API for user to search Google's book library, and save the book to MySQL database for laster use.</p>
+                            <p>A site that utilize Google Books API for user to search and save books as well as a ranking page to display top saved books across all users.</p>
+                            <p>&#9679; Using React and bootstrap for frontend; Sequelize and ExpressJS for backend.</p>
+                            <p>&#9679; JWT for user authorization, bcrypt to hash user password and dotenv to hide other sensitive data such as database username, password, etc.</p>
+                            <p>&#9679; Uses ChartJS to display top 10 saved books from all users.</p>
                             <p>GitHub Repo: <a href="https://github.com/Terry0532/Google-Books-Search" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://frozen-anchorage-90956.herokuapp.com/" target="_blank" rel="noopener noreferrer">Link</a></p>
                             <h6><i><b>WEATHER DASHBOARD</b></i></h6>
                             <p>
-                                A single page application built with "mobile first design" in mind. Uses OpenWeatherMap's API for weather informations.
-                                Attention to details suck as, provide user with their local time as well as the time of search result city;
-                                simple and intuitive GPS button for user's current location's weather;
-                                users local storage to save user's last searched city for faster access when user open the site again.
-                            </p>
+                                A simple site built with “mobile first design” in mind.
+                                Uses OpenWeatherMap’s api for weather informations.
+                                Details such as, provide user with their local time and the searched city’s time;
+                                a GPS button to get user’s current location;
+                                uses local storage to save last searched city for faster access when user open the site again.
+                           </p>
                             <p>GitHub Repo: <a href="https://github.com/Terry0532/Weather-Dashboard" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://terry0532.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">Link</a></p>
-                            <h6><i><b>BUDGET TRACKER</b></i></h6>
-                            <p>
-                                A simple budget tracker application with the functionality of offline access.
-                                Uses Service Worker to cache all the necessary files and IndexedDB to save user's transactions when the internet is not available.
-                            </p>
-                            <p>GitHub Repo: <a href="https://github.com/Terry0532/Budget-Trackers" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://protected-spire-05676.herokuapp.com/" target="_blank" rel="noopener noreferrer">Link</a></p>
                             <h3><b>Education</b></h3>
                             <p>Salt Lake Community College - General Education, 2012-2016</p>
                             <p>University of Utah - Film and Media Arts with Entertainment Arts and Engineering Emphasis BA, 2016-2019</p>
