6a329dc930138e6f0d506f40e936bc7ccaf02852
Terry0532 <t3rrygong@gmail.com>
2023-01-30 10:59:46 -0700

    udpate resume

--- src/components/Fan/fan.js
+++ src/components/Fan/fan.js
@@ -31,37 +31,79 @@ function handleRightClick() {
 function Fan(props) {
   return (
     <div className="container mt-2 fanbody">
-      <div className="card-swiper">
-        <div className="card-groups">
-          <div className="card-group" data-index="0" data-status="active">
-            <div className="little-card cards"></div>
-            <div className="big-card cards">1</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">2</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">3</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">4</div>
-          </div>
-          <div className="card-group" data-index="1" data-status="unknown">
-            <div className="little-card cards"></div>
-            <div className="big-card cards">5</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">6</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">7</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">8</div>
-          </div>
-          <div className="card-group" data-index="2" data-status="unknown">
-            <div className="little-card cards"></div>
-            <div className="big-card cards">9</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">10</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">11</div>
-            <div className="little-card cards"></div>
-            <div className="big-card cards">12</div>
+      <div className="col-md">
+        <h1>Portfolio</h1>
+        <hr />
+        <div id="test" className="test" tabIndex={-1}>
+          123
+          <button className="test2">321</button>
+        </div>
+        <br />
+        <div className="card-swiper">
+          <div className="card-groups">
+            <div className="card-group" data-index="0" data-status="active">
+              {/* <div className="little-card cards"></div>
+              <div className="big-card cards">1</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">2</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">3</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">4</div> */}
+              <div className="card-group-chess" tabIndex={-1}>
+                <div className="big-card cards projectChess">
+                  <img
+                    src="images/project_screenshots/online_chess.gif"
+                    alt="online_chess"
+                  />
+                </div>
+                <div className="big-card cards button1">
+                  <a
+                    href="https://warm-brushlands-64673.herokuapp.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="btn btn-primary mr-1"
+                    role="button"
+                  >
+                    Website
+                  </a>
+                </div>
+                <div className="big-card cards button2">
+                  <a
+                    href="https://github.com/Terry0532/ChessV2"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="btn btn-secondary"
+                    role="button"
+                  >
+                    GitHub Repository
+                  </a>
+                </div>
+              </div>
+              <p>An online chess game.</p>
+            </div>
+            <div className="card-group" data-index="1" data-status="unknown">
+              {/* <div className="little-card cards"></div>
+              <div className="big-card cards">5</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">6</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">7</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">8</div> */}
+              test
+            </div>
+            <div className="card-group" data-index="2" data-status="unknown">
+              {/* <div className="little-card cards"></div>
+              <div className="big-card cards">9</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">10</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">11</div>
+              <div className="little-card cards"></div>
+              <div className="big-card cards">12</div> */}
+              test2
+            </div>
           </div>
         </div>
         <div className="card-swiper-buttons">
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -3,53 +3,149 @@ import Navbar from "../components/Navbar/navbar";
 import resume from "../PDF/Resume.pdf";
 
 function Resume() {
-    return (
-        <div>
-            <Navbar about="nav-link" portfolio="nav-link" resume="nav-link active" />
-            <div className="container mt-2">
-                <div className="row">
-                    <div className="col-md">
-                        <h1>Resume</h1>
-                        <hr />
-                        <article>
-                            <h3><b>Skills</b></h3>
-                            <p>JavaScript, HTML, CSS, jQuery, React, NodeJS, ExpressJS, MySQL, Sequelize, MongoDB, Mongoose, AWS EC2, AWS RDS, Socket.IO</p>
-                            <h3><b>Projects</b></h3>
-                            <h6><i><b>ONLINE-CHESS</b></i></h6>
-                            <p>An online chess game.</p>
-                            <p>&#9679; Built server side with ExpressJS and Socket.IO for online multiplayer game.</p>
-                            <p>&#9679; React client side with all game logic, such as, en passant capture, stalemate, etc.</p>
-                            <p>&#9679; Need at least 2 clients to start the game.</p>
-                            <p>GitHub Repo: <a href="https://github.com/Terry0532/ChessV2" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://warm-brushlands-64673.herokuapp.com" target="_blank" rel="noopener noreferrer">Link</a></p>
-                            <h6><i><b>GOOGLE BOOKS SEARCH</b></i></h6>
-                            <p>A site that utilize Google Books API for user to search and save books, as well as a ranking page to display top saved books across all users.</p>
-                            <p>&#9679; React and bootstrap for frontend; Sequelize and ExpressJS for backend; AWS RDS for database.</p>
-                            <p>&#9679; JWT for user authorization, bcrypt to hash user password and dotenv to hide other sensitive data such as database username, password, etc.</p>
-                            <p>&#9679; Uses ChartJS to display top 10 saved books from all users.</p>
-                            <p>GitHub Repo: <a href="https://github.com/Terry0532/Google-Books-Search" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://frozen-anchorage-90956.herokuapp.com" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://frozen-anchorage-90956.herokuapp.com" target="_blank" rel="noopener noreferrer">Link</a></p>
-                            <h6><i><b>MYAUTOSPACE</b></i></h6>
-                            <p>
-                                MyAutoSpace is must have for any home mechanic.
-                                The simple interface allows users to document their vehicle histories and keep up with suggested maintenance.
-                           </p>
-                            <p>&#9679; Uses Google Map’s API to display car maintenance shop near user’s location and CarMD VIN Decoder API to provide information of their vehicles.</p>
-                            <p>&#9679; User can add new vehicle when they are offline, and it will automatically sync with the server when online.</p>
-                            <p>GitHub Repo: <a href="https://github.com/JINJ95/AutoSpace" target="_blank" rel="noopener noreferrer">Link</a> Deployed on Heroku: <a href="https://young-lake-34599.herokuapp.com/" target="_blank" rel="noopener noreferrer">Link</a></p>
-                            <h3><b>Education</b></h3>
-                            <p>Salt Lake Community College - General Education, 2012-2016</p>
-                            <p>University of Utah - Film and Media Arts with Entertainment Arts and Engineering Emphasis BA, 2016-2019</p>
-                            <a href="https://www.youracclaim.com/badges/fc3efed1-0c3b-4a45-94a1-6161b27a112d/public_url" target="_blank" rel="noopener noreferrer">University of Utah Professional Education — Full Stack Developer Certificate, 2020 Feb-Aug</a>
-                            <h3><b>Work Experiences</b></h3>
-                            <p>Warehouse Associate, Amazon; Salt Lake City, Utah — 2019-2020</p>
-                            <p>Delivery Driver, DoorDash; West Jordan, Utah - 2020-Present</p>
-                        </article>
-                        <a href={resume} target="_blank" rel="noopener noreferrer" className="btn btn-primary" role="button">Open in PDF</a>
-                        <hr />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <Navbar about="nav-link" portfolio="nav-link" resume="nav-link active" />
+      <div className="container mt-2">
+        <div className="row">
+          <div className="col-md">
+            <h1>Resume</h1>
+            <hr />
+            <article>
+              <h3>
+                <b>Skills</b>
+              </h3>
+              <p>
+                JavaScript, HTML, CSS, jQuery, Git,React, NodeJS, ExpressJS,
+                MySQL, Sequelize, MongoDB, Mongoose, AWS EC2, AWS RDS,
+                Socket.IO, Wordpress, PHP
+              </p>
+              <h3>
+                <b>Projects</b>
+              </h3>
+              <h6>
+                <i>
+                  <b>ONLINE-CHESS</b>
+                </i>
+              </h6>
+              <p>An online chess game.</p>
+              <p>
+                &#9679; Built server side with ExpressJS and Socket.IO for
+                online multiplayer game.
+              </p>
+              <p>
+                &#9679; React client side with all game logic, such as, en
+                passant capture, stalemate, etc.
+              </p>
+              <p>&#9679; Need at least 2 clients to start the game.</p>
+              <p>
+                GitHub Repo:{" "}
+                <a
+                  href="https://github.com/Terry0532/ChessV2"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>{" "}
+                Deployed on Heroku:{" "}
+                <a
+                  href="https://warm-brushlands-64673.herokuapp.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              </p>
+              <h6>
+                <i>
+                  <b>GOOGLE BOOKS SEARCH</b>
+                </i>
+              </h6>
+              <p>
+                A site that utilize Google Books API for user to search and save
+                books, as well as a ranking page to display top saved books
+                across all users.
+              </p>
+              <p>
+                &#9679; React and bootstrap for frontend; Sequelize and
+                ExpressJS for backend; AWS RDS for database.
+              </p>
+              <p>
+                &#9679; JWT for user authorization, bcrypt to hash user password
+                and dotenv to hide other sensitive data such as database
+                username, password, etc.
+              </p>
+              <p>
+                &#9679; Uses ChartJS to display top 10 saved books from all
+                users.
+              </p>
+              <p>
+                GitHub Repo:{" "}
+                <a
+                  href="https://github.com/Terry0532/Google-Books-Search"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>{" "}
+                Deployed on Heroku:{" "}
+                <a
+                  href="https://frozen-anchorage-90956.herokuapp.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              </p>
+              <h3>
+                <b>Education</b>
+              </h3>
+              <p>Salt Lake Community College - General Education, 2012-2016</p>
+              <p>
+                University of Utah - Film and Media Arts with Entertainment Arts
+                and Engineering Emphasis BA, 2016 - 2019
+              </p>
+              <p>
+                University of Utah Professional Education —
+                <a
+                  href="https://www.youracclaim.com/badges/fc3efed1-0c3b-4a45-94a1-6161b27a112d/public_url"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Full Stack Developer Certificate
+                </a>
+                , 2020 Feb - Aug
+              </p>
+              <h3>
+                <b>Work Experiences</b>
+              </h3>
+              <p>
+                Warehouse Associate, Amazon; Salt Lake City, Utah — 2019 - 2020
+              </p>
+              <p>
+                Delivery Driver, DoorDash; West Jordan, Utah - 2020 - Present
+              </p>
+              <p>Flex Driver, Amazon; Salt Lake City, Utah - 2022 - Present</p>
+              <p>
+                Web Developer Intern, Fusion360; Draper, Utah - 2022 Oct - 2023
+                Feb
+              </p>
+            </article>
+            <a
+              href={resume}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+              role="button"
+            >
+              Open in PDF
+            </a>
+            <hr />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
